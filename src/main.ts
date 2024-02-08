cc4d3b7e56f4039c44ff75a932c23a39b72b340b
SuperTyler <896267775@qq.com>
2024-02-08 11:00:15 +0800

    feat: 使用pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import * as Pinia from 'pinia'
+import { createUnistorage } from 'pinia-plugin-unistorage'
+
 export function createApp() {
+  // 创建 pinia 实例
+  const store = Pinia.createPinia()
+  // 持久化插件
+  store.use(createUnistorage())
+
   const app = createSSRApp(App)
+  app.use(store)
   return {
-    app
+    app,
+    Pinia
   }
 }
