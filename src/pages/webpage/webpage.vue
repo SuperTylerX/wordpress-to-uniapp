abf681efd4c499ae908a3a7fddf2749e5b66230a
Tianxiang Ying <896267775@qq.com>
2024-02-20 15:19:22 +0800

    feat: webview页面

---
+++ src/pages/webpage/webpage.vue
@@ -0,0 +1,27 @@
+<template>
+  <web-view :src="url"></web-view>
+</template>
+
+<script lang="ts">
+import { BASE_URL } from '@/config'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      url: ''
+    }
+  },
+  onLoad: function (options) {
+    if (!options) {
+      return
+    }
+    if (options.url) {
+      this.url = decodeURIComponent(options.url)
+    } else {
+      this.url = BASE_URL
+    }
+  }
+})
+</script>
+<style scoped lang="scss"></style>
